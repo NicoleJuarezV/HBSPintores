55fe66d015f5a187ec11cdc5f084feab9b00a8e7
NicoleJuarezV <65268721+NicoleJuarezV@users.noreply.github.com>
2020-06-01 22:44:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,36 +10,51 @@ const hbs = require('hbs');
 app.set('view engine', 'hbs');
 
 //Definimos donde estan los parciales
-hbs.registerPartials(__dirname + '/views/partials/');
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(express.static(__dirname + '/public'));
 
 //Definimos la ruta.
 app.get('/', (req,res)=>{
     res.render('index',{
+      autor : 'Nicole Juarez',
+        year : new Date().getFullYear(),
+        title : 'Inicio'
     });
 });
 
 app.get('/monet', (req,res)=>{
     res.render('monet',{
+      autor : 'Nicole Juarez',
+        year : new Date().getFullYear(),
+        title : 'Monet'
     });
 });
 
 app.get('/picass', (req,res)=>{
     res.render('picass',{
+      autor : 'Nicole Juarez',
+        year : new Date().getFullYear(),
+        title : 'Picasso'
     });
 });
 app.get('/velaz', (req,res)=>{
     res.render('velaz',{
+      autor : 'Nicole Juarez',
+        year : new Date().getFullYear(),
+        title : 'Velazquez'
     });
 });
 app.get('/dali', (req,res)=>{
     res.render('dali',{
+      autor : 'Nicole Juarez',
+        year : new Date().getFullYear(),
+        title : 'Dali'
     });
 });
 
 //Arrancamos el servidor web en el puerto 3000
-app.listen(3000,()=>{
+app.listen(8080,()=>{
     //Escribimos en la terminal
-    console.log('Escuchando en el puerto 3000');
+    console.log('Escuchando en el puerto 8080');
 });
