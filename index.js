6fde503b6d18fdd50a1c473c62db613dc8b17d82
NicoleJuarezV <65268721+NicoleJuarezV@users.noreply.github.com>
2020-06-01 22:18:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
+
+//Importamos el "paquete" express
 const express = require('express');
+//Creamos un Objeto de tipo express
+const app = express();
+//Importar HBS
 const hbs = require('hbs');
 
-const app = express();
-
+//Defino el motor"plantilla" con el que trabajare
 app.set('view engine', 'hbs');
 
-hbs.registerPartials(_dirname + '/views/partials/');
+//Definimos donde estan los parciales
+hbs.registerPartials(__dirname + '/views/partials/');
 
-app.use(express.static(_dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res)=>{
-    res.render('index');
+//Definimos la ruta.
+app.get('/', (req,res)=>{
+    res.render('index',{
+    });
 });
-app.get('/dali', (req, res)=>{
-    res.render('dali');
+
+app.get('/monet', (req,res)=>{
+    res.render('monet',{
+    });
 });
-app.get('/picass', (req, res)=>{
-    res.render('picasso');
+
+app.get('/picass', (req,res)=>{
+    res.render('picass',{
+    });
 });
-app.get('/velaz', (req, res)=>{
-    res.render('velazques');
+app.get('/velaz', (req,res)=>{
+    res.render('velaz',{
+    });
 });
-app.get('/monet', (req, res)=>{
-    res.render('monet');
+app.get('/dali', (req,res)=>{
+    res.render('dali',{
+    });
 });
-app.listen(3000, ()=>{
-    console.log('Escuchando el puerto 3000');
+
+//Arrancamos el servidor web en el puerto 3000
+app.listen(3000,()=>{
+    //Escribimos en la terminal
+    console.log('Escuchando en el puerto 3000');
 });
